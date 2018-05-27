4f8a9b9f72e1726401d9de96351993042cf10c6d
NeverStopLearning <expandinghorizonssi@gmail.com>
2018-05-27 12:07:13 -0400

    3 dumb components setup: Navbar, Landing. Footer. Setting up react router for navigations next

---
+++ client/fileTempsExamples/ClassBasedComponent.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+
+
+class componentName extents Component {
+	render(){
+		return(
+				<div>
+				
+				</div>
+		)
+	}
+}
+
+export default componentName;
+
+
+//or
+
+
+
+/*export default class componentName extents Component {
+	render(){
+		return(
+				<div>
+				
+				</div>
+		)
+	}
+}*/
+
